5f6a9b50dc545a2e3ada681b7902deda0b8bb8bd
Mike Stumpf <mstumpf@amfam.com>
2021-04-15 13:45:42 -0500

    added missing amfam states

--- modules/enums/amfam.py
+++ modules/enums/amfam.py
@@ -1,5 +1,5 @@
-amfam_territory_states = ['AZ', 'CO', 'IA', 'ID', 'IL', 'IN', 'KS', 'MO', 'MN', 'ND', 'NE', 'NV', 'OH', 'OR',
-                          'SD', 'UT', 'WI']
+amfam_territory_states = ['AZ', 'CO', 'GA', 'IA', 'ID', 'IL', 'IN', 'KS', 'MO', 'MN', 'ND', 'NE', 'NV', 'OH', 'OR',
+                          'SD', 'UT', 'WA', 'WI']
 
 amfam_only_states_indicator = 1
 all_states_indicator = 0
