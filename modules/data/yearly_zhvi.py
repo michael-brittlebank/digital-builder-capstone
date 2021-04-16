92641eb07115bc8a12312e0f7ed2f155c627081c
Mike Stumpf <mstumpf@amfam.com>
2021-04-16 07:57:58 -0500

    fixed calculate yearly zhvi bug

--- modules/data/yearly_zhvi.py
+++ modules/data/yearly_zhvi.py
@@ -1,9 +1,8 @@
-from ..enums import *
 from ..database import calculate_average_zhvi
 
 
 def calculate_yearly_zhvi():
     # calculate for all states
-    calculate_average_zhvi(all_states_indicator)
+    calculate_average_zhvi(False)
     # calculate amfam only
-    calculate_average_zhvi(amfam_only_states_indicator)
+    calculate_average_zhvi(True)
